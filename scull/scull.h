a362b04d767bc3c991a7070245d2b51fd7166ca6
qianqiangliu <qianqiangliu@hotmail.com>
2017-10-02 12:19:54 +0800

    add ioctl syscall

--- scull/scull.h
+++ scull/scull.h
@@ -1,14 +1,38 @@
 #ifndef _SCULL_H_
 #define _SCULL_H_
 
+#ifdef __KERNEL__
 #include <linux/semaphore.h>
 #include <linux/cdev.h>
+#include <linux/ioctl.h>
+#endif /* __KERNEL__ */
 
-#define SCULL_MAJOR	0
-#define SCULL_NR_DEVS	4
-#define SCULL_QUANTUM	4096
-#define SCULL_QSET	1024
+#define SCULL_MAJOR		0
+#define SCULL_NR_DEVS		4
+#define SCULL_QUANTUM		4096
+#define SCULL_QSET		1024
 
+/* ioctl commands */
+#define SCULL_IOC_MAGIC		'l'
+
+#define SCULL_IOCRESET		_IO(SCULL_IOC_MAGIC, 0)
+
+#define SCULL_IOCSQUANTUM	_IOW(SCULL_IOC_MAGIC, 1, int)
+#define SCULL_IOCSQSET		_IOW(SCULL_IOC_MAGIC, 2, int)
+#define SCULL_IOCTQUANTUM	_IO(SCULL_IOC_MAGIC, 3)
+#define SCULL_IOCTQSET		_IO(SCULL_IOC_MAGIC, 4)
+#define SCULL_IOCGQUANTUM	_IOR(SCULL_IOC_MAGIC, 5, int)
+#define SCULL_IOCGQSET		_IOR(SCULL_IOC_MAGIC, 6, int)
+#define SCULL_IOCQQUANTUM	_IO(SCULL_IOC_MAGIC, 7)
+#define SCULL_IOCQQSET		_IO(SCULL_IOC_MAGIC, 8)
+#define SCULL_IOCXQUANTUM	_IOWR(SCULL_IOC_MAGIC, 9, int)
+#define SCULL_IOCXQSET		_IOWR(SCULL_IOC_MAGIC, 10, int)
+#define SCULL_IOCHQUANTUM	_IO(SCULL_IOC_MAGIC, 11)
+#define SCULL_IOCHQSET		_IO(SCULL_IOC_MAGIC, 12)
+
+#define SCULL_IOC_MAXNR		14
+
+#ifdef __KERNEL__
 struct scull_qset {
 	void **data;
 	struct scull_qset *next;
@@ -23,5 +47,6 @@ struct scull_dev {
 	struct semaphore sem;		/* 互斥信号量 */
 	struct cdev cdev;		/* 字符设备结构 */
 };
+#endif /* __KERNEL__ */
 
 #endif /* _SCULL_H_ */
---
+++ scull/user.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+#include "scull.h"
+
+int main()
+{
+	int fd;
+
+	fd = open("/dev/scull0", O_RDONLY);
+	if (fd < 0) {
+		perror("open()");
+		return 1;
+	}
+
+	int quantum;
+	int ret = ioctl(fd, SCULL_IOCGQUANTUM, &quantum);
+	if (ret < 0) {
+		perror("ioctl()");
+		return 1;
+	}
+	printf("quantum: %d\n", quantum);
+
+	int qset = ioctl(fd, SCULL_IOCQQSET, &qset);
+	if (qset < 0) {
+		perror("ioctl()");
+		return 1;
+	}
+	printf("qset: %d\n", qset);
+
+	return 0;
+}
